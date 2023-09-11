ba880b8e72f8c2e47a2f3fab8a31ee51fc417493
kawasaki325 <129140889+kawasaki325@users.noreply.github.com>
2023-09-11 17:42:34 +0900

    タイトル変更

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>旅行の日程管理</b>',
+    'logo' => '<b>旅行日程管理</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => '旅行プラン管理',
+    'logo_img_alt' => '旅行日程管理',
 
     /*
     |--------------------------------------------------------------------------
